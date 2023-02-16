2937828a2c74056ece83446260330130496f421f
Aggrey-Osango <hurrycane661@gmail.com>
2023-02-16 14:17:33 +0000

    Commit it

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
+
+/*Puts functions that is used to print things in one line  */
 int main(void)
 {
-	/*Puts functions that is used to print things in one line  */
 	puts("\"Programming is like building a multilingual puzzle\n");
 
 return (0);
