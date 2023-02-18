ee06b9cb52887daf100d55064740bf4ca362e586
Aggrey-Osango <hurrycane661@gmail.com>
2023-02-18 00:37:47 +0000

    Printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
-
-int main(){
-
+/**
+ * main - The main entry point of a function
+ * @void: takes no paremeter
+ *
+ * Return: after successfully compeleting
+ */
+int main(void)
+{
 	printf("with proper grammar, but the outcome is a piece of art\n");
-
-return 0;
+return (0);
 }
