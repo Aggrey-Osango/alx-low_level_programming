6096770a54605c0bd86c38e55faa4af75406a703
Aggrey-Osango <hurrycane661@gmail.com>
2023-02-18 01:17:08 +0000

    Intel  Assembly code

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,3 +1,14 @@
 #include <stdio.h>
 
-int main(void){return 0;}
+/**
+ *main - The main program entry
+ *@void: does not accept arguments
+ *
+ * Return: code 0 after runs successfully
+ */
+
+int main(void)
+{
+	printf("Hello World");
+return (0);
+}
