d9ba58b14b8c22832e5e7170f97e88fac740bc7d
Aggrey-Osango <hurrycane661@gmail.com>
2023-02-18 00:30:31 +0000

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
-/*
- * function_name => main, the main entry point of a program
- * @parameters => void, it does not take any parameter
+/**
+ * main - the main entry point of a program
+ * @void: takes not parameter
  *
- * Return 0: when the code is executed successfully it returns the code 0
- * */
+ * Return: After a successful completition
+ *
+ */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
-
-return (0)
+return (0);
 }
